5b3a794e0ca866365dae4c56a807ef32479d2d2a
Vishnu1119v <105714872+Vishnu1119v@users.noreply.github.com>
2022-11-25 23:03:11 +0530

    Question 2

---
+++ string_inside_function.py
@@ -0,0 +1,8 @@
+str1=input('enter the string :')
+def rev(str1):
+    res=''
+    for ch in str1:
+        res=ch+res
+    return res
+print('The original string =',str1)
+print('reverse of the given string =',rev(str1))
